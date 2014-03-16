0509ebdc1f2db57ecc540bcdf509f277a27ccd10
kcomputer <emil.ildarovich@gmail.com>
2014-03-17 00:09:31 +0400

    Added support of other bracket kinds

--- semester_4/3/task_6/bracketsCheck.hs
+++ semester_4/3/task_6/bracketsCheck.hs
@@ -2,17 +2,21 @@ import Test.QuickCheck
 
 bracketsCheck :: String -> Bool
 
-bracketsCheck xs = bracketsCheckHelper xs 0
+bracketsCheck xs = bracketsCheckHelper xs [0,0,0]
 
-bracketsCheckHelper :: String -> Int -> Bool
+bracketsCheckHelper :: String -> [Integer] -> Bool
 
-bracketsCheckHelper [] acc = acc == 0
+bracketsCheckHelper [] acc = acc == [0,0,0]
 bracketsCheckHelper (x:xs) acc = case x of
-								'(' -> bracketsCheckHelper xs (acc + 1)
-								')' -> bracketsCheckHelper xs (acc -1)
+								'(' -> bracketsCheckHelper xs (zipWith (+) [1,0,0] acc)
+								')' -> bracketsCheckHelper xs (zipWith (-) [1,0,0] acc)
+								'{' -> bracketsCheckHelper xs (zipWith (+) [0,1,0] acc)
+								'}' -> bracketsCheckHelper xs (zipWith (-) [0,1,0] acc)
+								'[' -> bracketsCheckHelper xs (zipWith (+) [0,0,1] acc)
+								']' -> bracketsCheckHelper xs (zipWith (-) [0,0,1] acc)
 								otherwise -> bracketsCheckHelper xs acc 
 
-bracketsCheckTest = bracketsCheck "()dsdsds(dd)" == True
-					&& bracketsCheck "((ddw" == False
+bracketsCheckTest = bracketsCheck "()dsds{d}[]s(dd)" == True
+					&& bracketsCheck "((ddw))]" == False
 
 main = quickCheck bracketsCheckTest
